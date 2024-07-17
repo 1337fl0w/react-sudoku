d122319f92e17cdb37f7228577abd089d3d18fe0
Florian Setzer <florian.setzer@robur-automation.com>
2024-07-17 15:26:31 +0200

    MAde app into PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+      manifest: {
+        name: "Sudoku",
+        short_name: "Sudoku",
+        description: "Sudoku",
+        theme_color: "#ffffff",
+        display: "standalone",
+        icons: [
+          {
+            src: "/favicon.ico",
+            sizes: "64x64 48x48 32x32 24x24 16x16",
+            type: "image/x-icon",
+          },
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
